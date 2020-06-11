3513081085b4abbcf299424a0075f65f44c47368
tokcum <tobias.mucke@gmail.com>
2020-06-11 14:53:46 +0200

    Add web server to provide REST API. Implement POST.

--- ws/src/main.rs
+++ ws/src/main.rs
@@ -1,10 +1,19 @@
 use model::ObjectType;
 use appl::pass;
 
-fn main() {
+use actix_web::{post, App, HttpServer, Responder, web};
+
+#[post("/api/{resource}")]
+async fn post(info: web::Path<String>) -> impl Responder {
     let ot = ObjectType::new();
-
     pass(&ot);
+    format!("ObjectType: {:?}", ot)
+}
 
-    println!("ObjectType: {:?}", ot);
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(post))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
